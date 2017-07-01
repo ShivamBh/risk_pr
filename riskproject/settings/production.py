139ab27bb6f7803731f52a9d00cf0eddc5eb7ea1
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-07-02 04:29:58 +0530

    production vars updated

--- riskproject/settings/production.py
+++ riskproject/settings/production.py
@@ -3,11 +3,10 @@ import dj_database_url
 
 
 from .base import *
-
 DEBUG = True
-SECRET_KEY = '8w@+-dis^dpj-e35xb1twx*#gg%l^nrvv+d&ijlz72^&k_j%-r'
 
-ALLOWED_HOSTS = ["issdemo.herokuapp.com"]
+
+ALLOWED_HOSTS = ['localhost', 'issdemo.herokuapp.com']
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env) 
