379bcf59d0551c61ad54a08b1150faaa40d8f12f
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-07-01 12:06:23 +0530

    fileField added to Country model for flag uploads, archive field added to Report model

--- reports/models.py
+++ reports/models.py
@@ -8,12 +8,41 @@ class Country(models.Model):
 		('H', 'High'),
 		('Ex', 'Extreme'),
 	)
+	SEC_RATING_CHOICES = (
+		('Green', 'Green'),
+		('Yellow', 'Yellow'),
+		('Amber', 'Amber'),
+		('Red', 'Red'),
+	)
+
 	name = models.CharField(max_length=100, blank=False, null=False)
-	#flag = models.ImageField(upload_to='flags')
+	flag = models.FileField(upload_to='flags', null=True)
 	rating = models.CharField(
 		max_length=2,
 		choices = RATING_CHOICES,
+		null=True,
 	)
+	sec_rating = models.CharField(
+		max_length=2,
+		choices = RATING_CHOICES,
+		null=True,
+	)
+	terror_rating = models.CharField(
+		max_length=2,
+		choices = RATING_CHOICES,
+		null=True,
+	)
+	travel_rating = models.CharField(
+		max_length=2,
+		choices = RATING_CHOICES,
+		null=True,
+	)
+	sec_level = models.CharField(
+		max_length=10,
+		choices = SEC_RATING_CHOICES,
+		null=True,
+	)
+
 	risk_bg = models.TextField()
 
 	def __str__(self):
@@ -30,11 +59,11 @@ class Report(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	title = models.CharField(max_length=500, blank=False, null=False)
-	summary = models.TextField()
+	summary = models.TextField(blank=False, null=False)
 	assessment = models.TextField()
-	impact_tc = models.CharField(max_length=500, blank=False, null=False)
+	impact_tc = models.CharField(max_length=500)
 	advice = models.TextField()
-	event_bg_title = models.CharField(max_length=300, blank=False, null=False)
+	event_bg_title = models.CharField(max_length=300)
 	event_bg_text = models.TextField()
 	
 	latitude = models.DecimalField(max_digits=10, decimal_places=4)
@@ -42,9 +71,10 @@ class Report(models.Model):
 	impact_radius = models.IntegerField()
 	report_type = models.CharField(
 		max_length=3,
-		choices=SUB_MODEL_CHOICES,
+		choices=SUB_MODEL_CHOICES, 
 		default="T",
 	)
+	archived = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.title
--- riskproject/urls.py
+++ riskproject/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf import settings 
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -29,3 +30,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
