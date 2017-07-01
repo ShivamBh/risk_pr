0526a6589e655cc17505271927f6c515a0acd04f
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-07-01 15:53:17 +0530

    productions settings init

--- riskproject/settings/production.py
+++ riskproject/settings/production.py
@@ -1,8 +1,17 @@
 from __future__ import absolute_import, unicode_literals
+import dj_database_url
+
 
 from .base import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = ["issdemo.herokuapp.com"]
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env) 
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 try:
     from .local import *
