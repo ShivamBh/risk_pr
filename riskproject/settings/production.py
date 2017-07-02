0d1b2a3ff1e3e7f14d3a56155b7ba943e611e3ef
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-07-03 01:40:42 +0530

    secret vars removed from source control

--- riskproject/settings/production.py
+++ riskproject/settings/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 from .base import *
 DEBUG = True
-SECRET_KEY = '8w@+-dis^dpj-e35xb1twx*#gg%l^nrvv+d&ijlz72^&k_j%-r'
+
 
 ALLOWED_HOSTS = ['localhost', 'issdemo.herokuapp.com']
 
