eadeea45a67cea1b78eefd5167fbc6a55199e427
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2017-08-31 17:35:49 +0530

    front end changes, reports-detail tabs added

--- riskproject/static/js/risk.js
+++ riskproject/static/js/risk.js
@@ -1,66 +1,48 @@
-$(document).ready(function() {
+function main() {
 
-
-
-
-
+	//tabs
+	
+	
+	
 	
 
 // Rating and color
-var ratings_text = $(".ratings-hidden");
-var ratings_color = $(".rating-color");
+	var ratings_text = $(".ratings-hidden");
+	var ratings_color = $(".rating-color");
 
 
-ratings_color.each(function() {
+	ratings_color.each(function() {
 
-	if ($(this).find("h6").text() == "L") {
-		$(this).css("background-color", "#62D277");
-	}
-	else if ($(this).find("h6").text() == "M") {
-		$(this).css("background-color", "#E5D643");
-	}
-	else if ($(this).find("h6").text() == "H") {
-		$(this).css("background-color", "#E68C58");
-	}
-	else {
-		$(this).css("background-color", "#C73542");
-	}
-	
-	console.log($(this).find("h6").text());
-});
+		if ($(this).find("h6").text() == "L") {
+			$(this).css("background-color", "#62D277");
+		}
+		else if ($(this).find("h6").text() == "M") {
+			$(this).css("background-color", "#E5D643");
+		}
+		else if ($(this).find("h6").text() == "H") {
+			$(this).css("background-color", "#E68C58");
+		}
+		else {
+			$(this).css("background-color", "#C73542");
+		}
+		
+		console.log($(this).find("h6").text());
+	});
 
-var sec_level = $(".sec-level-rating-text h3");
-var sec_color = $("sec-level-rating-color");
+	var sec_level = $(".sec-level-rating-text h3");
+	var sec_color = $("sec-level-rating-color");
 
-	if (sec_level.text() === "Green") {
-		$("sec-level-rating-color").css("background-color", "C73542");
-	}
-
-	$( function() {
-		$( "#datepicker_from" ).datepicker({dateFormat: "yy-mm-dd"});
-    $( "#datepicker_to" ).datepicker({dateFormat: "yy-mm-dd"});
-  } );
-
-//console.log($("sec-level-rating-color"));
- });
-
-function openTab(e, tabName) {
-
-		e.stopPropagation();
-		var prevTab;
-		var targetTab = $(e.target);
-		var tabs = $(".tab-links");
-		var targetDiv = $("#"+tabName);
-
-		for(var i = 0; i < tabs.length; i++) {
-			if($(tabs[i]).hasClass("active")) {
-				prevTab = tabs[i];
-			}
+		if (sec_level.text() === "Green") {
+			$("sec-level-rating-color").css("background-color", "C73542");
 		}
 
-		$(prevTab).removeClass("active");
-		targetTab
+		$( function() {
+			$( "#datepicker_from" ).datepicker({dateFormat: "yy-mm-dd"});
+	    	$( "#datepicker_to" ).datepicker({dateFormat: "yy-mm-dd"});
+	  } );
+
+	//console.log($("sec-level-rating-color"));
+	 }
 
 
-
-	}
+window.onload = main;
