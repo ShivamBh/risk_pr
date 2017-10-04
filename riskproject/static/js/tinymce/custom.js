03828a22b46e4aab7769e7590dad8930a1667666
Shivam <shivam.bhattacharjee94@gmail.com>
2017-10-04 18:27:02 +0530

    user uploaded images added to tinymce

--- riskproject/static/js/tinymce/custom.js
+++ riskproject/static/js/tinymce/custom.js
@@ -8,9 +8,22 @@ tinymce.init({
     'insertdatetime media table contextmenu paste code help'
   ],
   toolbar: 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-  file_browser_callback: function(field_name, url, type, win) {
-    win.document.getElementById(field_name).value = 'my browser value';
-  }
+  file_picker_callback: function(callback, value, meta) {
+      if (meta.filetype == 'image') {
+        $('#upload').trigger('click');
+        $('#upload').on('change', function() {
+          var file = this.files[0];
+          var reader = new FileReader();
+          reader.onload = function(e) {
+            callback(e.target.result, {
+              alt: ''
+            });
+          };
+          reader.readAsDataURL(file);
+        });
+      }
+    },
+    
   // content_css: [
   //   '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
   //   '//www.tinymce.com/css/codepen.min.css']
