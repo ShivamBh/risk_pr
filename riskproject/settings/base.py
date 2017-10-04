881ef08b73aeeb9cc83747a53a02bb3169ce15d1
Shivam <shivam.bhattacharjee94@gmail.com>
2017-10-04 18:13:04 +0530

    emailmsg objects, trial sub stats, file browser tinymce

--- riskproject/settings/base.py
+++ riskproject/settings/base.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 # celery config
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-# CELERY_BROKER_URL = os.environ['REDIS_URL']
-# CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
