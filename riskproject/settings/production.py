547587c304f8adb70918a13c4ae523ac04af326e
mukeshsharma <pksharma341989@gmail.com>
2018-05-05 15:42:37 +0530

    back to changes

--- riskproject/settings/production.py
+++ riskproject/settings/production.py
@@ -13,8 +13,7 @@ TWILIO_PHONE_NUMBER = '+14159149102'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_HOST_USER = 'ShivamBh'
-EMAIL_HOST_USER = 'info@issrisk.com'
+EMAIL_HOST_USER = 'ShivamBh'
 EMAIL_HOST_PASSWORD = 'sendgrid#123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
