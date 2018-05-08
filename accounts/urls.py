3147721a0c48c060353096b20a24d10ac0177ef0
mukeshsharma <pksharma341989@gmail.com>
2018-05-08 14:27:48 +0530

    Authentication with token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import index
+from .views import index,tokenurl
 
 urlpatterns = [
 	 url(r'^$', index, name='home'),
+	 url('generatetokenurl', tokenurl, name='generatetokenurl'),
 	
 ]
